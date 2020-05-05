286c38e001a3ce90f81220d295195744ee42d50b
Witti <Witti@DESKTOP-DDU0R3T>
2020-05-05 16:03:29 +0200

    MonthlyTimeSeriesModel

---
+++ StockManager/StockDataServices/Models/MonthlyTimeSeries.cs
@@ -0,0 +1,11 @@
+﻿namespace StockDataServices.Models
+{
+    internal class MonthlyTimeSeries:IModel
+    {
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Price { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
