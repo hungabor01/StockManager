ce3f2922ba1c5bfce94f818a8aa99602d54c8c51
Witti <Witti@DESKTOP-DDU0R3T>
2020-05-06 19:45:11 +0200

    DailyTimeSeriesParameter

---
+++ StockManager/StockDataServices/Parameters/DailyTimeSeriesParameter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockDataServices.Parameters
+{
+    internal class DailyTimeSeriesParameter : IParameter
+    {
+        public string UrlPattern { get; } = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={1}&apikey={0}&datatype=csv";
+
+        public string Symbol { get; set; }
+
+        public DailyTimeSeriesParameter(string symbol)
+        {
+            Symbol = symbol;
+        }
+
+    }
+}
