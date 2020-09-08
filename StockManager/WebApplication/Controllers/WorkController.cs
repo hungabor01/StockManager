5b5ceba3fefc60992e69d8e2e981c3d4c3c56361
wittibalint <wittibalint99@gmail.com>
2020-09-08 11:01:38 +0200

    Search_function

---
+++ StockManager/WebApplication/Controllers/WorkController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StockDataServices.DataServices;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [Controller]
+    [Route("/api/Work/search/")]
+    public class WorkController : Controller
+    {
+        private readonly IStockClient client;
+        private  IEnumerable<StockSearch> stockSearch = new List<StockSearch>();
+        public IActionResult Work()
+        {
+            return View();
+        }
+        public WorkController(IStockClient client)
+        {
+            this.client = client;
+        }
+
+        [HttpGet("{stockName}")]
+        public ActionResult Work(string stockName)
+        {
+            if(stockName == null){
+                return View();
+            }
+            List<string[]> stocks = client.Search(stockName);
+           
+            foreach (var item in stocks)
+            {
+                stockSearch = stockSearch.Append(new StockSearch {
+                    Symbol = item[0],
+                    Region = item[1],
+                    Name   = item[2],
+                    Currenc= item[3]
+                });
+            }
+            ViewData.Model = stockSearch;
+
+            return View();
+        }
+    }
+}
---
+++ StockManager/WebApplication/Models/StockSearch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class StockSearch
+    {
+        public string Symbol{ get ;set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string Currenc { get; set; }
+    }
+}
