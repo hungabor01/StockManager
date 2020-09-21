b8d35d045106509a44b9a1a086908725d2e61e98
hungabor01 <hungabor01@gmail.com>
2020-09-21 21:36:42 +0200

    little refactoring

--- StockManager/WebApplication/Controllers/WorkController.cs
+++ StockManager/WebApplication/Controllers/WorkController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StockDataServices.DataServices;
 using WebApplication.Models;
@@ -12,68 +11,70 @@ namespace WebApplication.Controllers
     [Route("/api/Work/search/")]
     public class WorkController : Controller
     {
-        private readonly IStockClient client;
-        private  IEnumerable<StockSearch> stockSearch = new List<StockSearch>();
-        private IEnumerable<GlobalQuote> globalQuote = new List<GlobalQuote>();
+        private readonly IStockClient _client;
+        private  IEnumerable<StockSearch> _stockSearch = new List<StockSearch>();
+        private IEnumerable<GlobalQuote> _globalQuote = new List<GlobalQuote>();
+
+        public WorkController(IStockClient client)
+        {
+            _client = client;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
-        public WorkController(IStockClient client)
-        {
-            this.client = client;
-        }
-
+        
         [HttpGet("{stockName}")]
         public ActionResult Index(string stockName)
         {
-            if(stockName == null){
+            if(string.IsNullOrWhiteSpace(stockName)){
                 return View();
             }
-            List<string[]> stocks = client.Search(stockName);
+
+            var stocks = _client.Search(stockName);
            
             foreach (var item in stocks)
             {
-                stockSearch = stockSearch.Append(new StockSearch {
+                _stockSearch = _stockSearch.Append(new StockSearch {
                     Symbol = item[0],
                     Name = item[1],
                     Region   = item[2],
                     Currenc= item[3]
                 });
             }
-            ViewData.Model = stockSearch;
 
+            ViewData.Model = _stockSearch;
             return View();
         }
+
         [HttpGet("StockPriceData")]
         public ActionResult StockPriceData()
         {
             var stockValue = Request.Query["Symbol"].ToString();
-            if (stockValue == null)
+            if (string.IsNullOrWhiteSpace(stockValue))
             {
                 return View();
             }
-            List<string[]> stocks = client.GetStockPriceData(stockValue);
 
-            foreach (var item in stocks)
+            var stock = _client.GetStockData(stockValue);
+
+            _globalQuote = _globalQuote.Append(new GlobalQuote
             {
-                globalQuote = globalQuote.Append(new GlobalQuote
-                {
-                            Symbol = item[0],
-                            Open = Decimal.Parse(item[1]),
-                            High = Decimal.Parse(item[2]),
-                            Low = Decimal.Parse(item[3]),
-                            Price=Decimal.Parse(item[4]),
-                            Volume = Decimal.Parse(item[5]),
-                            LatestTradingDay=item[6],
-                            PreviousClose = Decimal.Parse(item[7]),
-                            Change = Decimal.Parse(item[8]),
-                            ChangePercent = item[9]
-                });
-            }
-            ViewData.Model = globalQuote;
+                Symbol = stock[0],
+                Open = decimal.Parse(stock[1]),
+                High = decimal.Parse(stock[2]),
+                Low = decimal.Parse(stock[3]),
+                Price= decimal.Parse(stock[4]),
+                Volume = decimal.Parse(stock[5]),
+                LatestTradingDay= stock[6],
+                PreviousClose = decimal.Parse(stock[7]),
+                Change = decimal.Parse(stock[8]),
+                ChangePercent = stock[9]
+            });
 
+            ViewData.Model = _globalQuote;
             return View();
         }
     }
